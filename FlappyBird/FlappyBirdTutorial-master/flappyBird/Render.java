caa6cfc3d530265d412960b64164927feb83c2c1
Veysel Bulut <veysel.bult@gmail.com>
2020-03-22 23:17:48 +0100

    FlappyBird

---
+++ FlappyBird/FlappyBirdTutorial-master/flappyBird/Render.java
@@ -0,0 +1,20 @@
+package flappyBird;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Render extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		MainFlappyBird.flappyBird.repaint(g);
+	}
+	
+}
